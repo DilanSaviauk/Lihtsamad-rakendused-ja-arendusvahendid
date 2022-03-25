7a4c422a2c6f64f89606d92d66e5a61bd2b63e6b
DilanSaviauk <dilan.saviauk@tptlive.ee>
2022-03-25 08:34:55 +0200

    aaaaa

--- lesson-2-game.py
+++ lesson-2-game.py
@@ -16,23 +16,23 @@ map = map2
 start_pos_x = 0
 start_pos_y = 0
 
-def move(step, current_position_x,current_position_y):
+def move(step, current_pos_x,current_pos_y):
     print("Current step is: ", step)
-    print("Current current_position_y = ", current_position_y)
-    print("Current current_position_x = ", current_position_x)
-    can_move_right = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 0
-    can_move_to_bottom =current_position_y <= 2 and map[current_position_y+1][current_position_x] == 0
+    print("Current current_position_y = ", current_pos_y)
+    print("Current current_position_x = ", current_pos_x)
+    can_move_right = current_pos_x <= 2 and map[current_pos_y][current_pos_x+1] == 0
+    can_move_down =current_pos_y <= 2 and map[current_pos_y+1][current_pos_x] == 0
    
     if can_move_right:
         print("Should move right")
-        current_position_x = current_position_x + 1
-    if can_move_to_bottom:
+        current_pos_x = current_pos_x + 1
+    if can_move_down:
         print("Should move down")
-        current_position_y = current_position_y + 1
+        current_pos_y = current_pos_y + 1
 
-    return [current_position_x, current_position_y]
+    return [current_pos_x, current_pos_y]
 
-new_position = move(1, start_position_x, start_position_y)
+new_position = move(1, start_pos_x, start_pos_y)
 new_position = move(2, new_position[0], new_position[1])
 new_position = move(3, new_position[0], new_position[1])
 
